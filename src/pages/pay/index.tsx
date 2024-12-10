5e13cbd3983dcfa313f046c0055ae6963744f32a
Pazy <pazymonet@gmail.com>
2024-12-10 16:07:56 +0100

    Linking again

--- src/pages/pay/index.tsx
+++ src/pages/pay/index.tsx
@@ -175,21 +175,24 @@ export default function Pay(): React.JSX.Element {
                       setCurrentPage("wallet-payment");
                     }
 
-                    if (resp?.data?.data?.checkout_link) {
-                      dispatch(setButtonClicked(true));
-
-                      dispatch(setP2PEscrowDetails(resp.data));
-                      setCurrentPage("escrow-page");
-                      return;
-                    }
+                  
 
                     if (resp.data?.escrow_status === 1) {
                       dispatch(setButtonClicked(true));
 
                       dispatch(setP2PEscrowDetails(resp.data));
 
-                      window.location.href = resp.data.data.checkout_link; // Redirect to the checkout link
-                      
+                      // Redirect to the checkout link
+                      if (resp?.data?.data?.checkout_link) {
+                        dispatch(setButtonClicked(true));
+                        
+                        dispatch(setP2PEscrowDetails(resp.data));
+                        window.location.href = resp.data.data.checkout_link;
+                        // setCurrentPage("escrow-page");
+                        return;
+                      }else{
+                        setCurrentPage("pay/v");
+                      }
 
                       // window.location.href = resp?.data?.data?.checkout_link;
 
