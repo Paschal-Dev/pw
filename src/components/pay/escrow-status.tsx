8f9f1552ce1b3cd2d703bb8b537354cf402bd5eb
Pazy <pazymonet@gmail.com>
2024-12-16 21:11:13 +0100

    Assign correction

--- src/components/pay/escrow-status.tsx
+++ src/components/pay/escrow-status.tsx
@@ -146,7 +146,7 @@ export default function EscrowStatus(){
             if (!localStorage.getItem('checkout_link')) {
               // Store a flag to prevent repeated redirection
               // localStorage.setItem('redirected', 'true');
-              console.log("The Unique Id =>>>", respo2.data.unique_id)
+              console.log("The Unique Id =>>>", respo2.data.data.unique_id)
               // window.location.assign(`https://pay.pwat.net/?v=${respo2.data.unique_id}`);
             }
 
