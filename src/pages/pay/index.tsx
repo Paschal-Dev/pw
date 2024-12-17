ceb9f24d06d2b5c09760fc4b7957367c57555c46
Pazy <pazymonet@gmail.com>
2024-12-17 09:38:04 +0100

    Assign revert

--- src/pages/pay/index.tsx
+++ src/pages/pay/index.tsx
@@ -145,7 +145,7 @@ export default function Pay(): React.JSX.Element {
               console.log("API RESPONSE FROM SEND OTP", resp.data);
 
 
-              // if (resp?.data?.data?.checkout_link) {
+              if (resp?.data?.data?.checkout_link) {
                 const checkoutLink = resp.data.data.checkout_link;
                 console.log("Redirecting to Checkout Link:", checkoutLink);
 
@@ -179,23 +179,23 @@ export default function Pay(): React.JSX.Element {
 
                 if (localStorage.getItem('checkout_link')) {
                   // Store a flag to prevent repeated redirection
-                  // Dispatch actions
                   // localStorage.setItem('redirected', 'true');
                   window.location.assign(checkoutLink);
-                  dispatch(setButtonClicked(true));
-                  dispatch(setP2PEscrowDetails(resp.data));
-  
-                  dispatch(setCurrentPage("escrow-page"));
                 } else {
                   // Redirection has already occurred; no query string manipulation needed
                   localStorage.removeItem('checkout_link'); // Clean up if needed
                 }
+                // Dispatch actions
+                dispatch(setButtonClicked(true));
+                dispatch(setP2PEscrowDetails(resp.data));
+
+                dispatch(setCurrentPage("escrow-page"));
                 
-                // return;
-              // } else {
+                return;
+              } else {
                 // console.log("No checkout link found in response.");
                 localStorage.clear();
-              // }
+              }
 
               // if (resp?.data?.data?.checkout_link) {
               //   dispatch(setButtonClicked(true));
