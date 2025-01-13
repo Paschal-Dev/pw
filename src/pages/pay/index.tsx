5a81713cd6d0d7783b83e07cc353d8cae3476223
Pazy <pazymonet@gmail.com>
2025-01-13 16:02:40 +0100

    Code Update

--- src/pages/pay/index.tsx
+++ src/pages/pay/index.tsx
@@ -80,14 +80,13 @@ export default function Pay(): React.JSX.Element {
           } else if (localStorage.getItem("redirectHandled") === "true")
             {
             // If already redirected, show the escrow page
-            localStorage.removeItem("redirectHandled");
-            localStorage.setItem("redirectHandled", "true");
-            window.location.assign(checkoutLink);
+            // localStorage.removeItem("redirectHandled");
+            // localStorage.setItem("redirectHandled", "true");
+            // window.location.assign(checkoutLink);
             console.log("Already redirected, displaying escrow page.");
             dispatch(setButtonClicked(true));
             dispatch(setCurrentPage("escrow-page"));
             dispatch(setP2PEscrowDetails(resp.data));
-            return;
           }
         } else {
           console.log("No checkout link available.");
