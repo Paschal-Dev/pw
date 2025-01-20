960559e562344886677a1e6f1d82e4b99e1cacaf
Pazy <pazymonet@gmail.com>
2025-01-20 07:41:30 +0100

    Code Update

--- src/pages/pay/index.tsx
+++ src/pages/pay/index.tsx
@@ -137,10 +137,10 @@ export default function Pay(): React.JSX.Element {
           .then((resp) => {
             console.log("Wallet Payment Status >>>", data?.wallet_pay?.payment_status);
             console.log("P2P Payment Status >>>", data?.pay?.payment_status);
-            if ([0, 1, 2, 3, 5].includes(resp.data?.wallet_pay?.payment_status)) {
+            if ([0, 1, 2, 3, 5].includes(data?.wallet_pay?.payment_status)) {
               dispatch(setWalletPaymentDetails(resp.data));
               dispatch(setCurrentPage("wallet-payment"));
-            }else if (resp?.data?.pay?.payment_status === 1) {
+            }else if (data?.pay?.payment_status === 1) {
               dispatch(setP2PEscrowDetails(resp.data));
               dispatch(setCurrentPage("p2p-payment"));
             }
